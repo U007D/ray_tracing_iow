804c94934e1a11d809193a7fe5df8c26707ecea8
u007d <b2b@humanenginuity.com>
2020-07-10 07:25:52 -0700

    ray_tracing_iow initial commit

---
+++ src/main.rs
@@ -0,0 +1,32 @@
+// Safety-critical application lints
+#![deny(
+    bare_trait_objects,
+    clippy::float_cmp_const,
+    clippy::indexing_slicing,
+    clippy::integer_arithmetic,
+    clippy::option_unwrap_used,
+    clippy::result_unwrap_used,
+    clippy::pedantic
+)]
+#![warn(clippy::all, clippy::nursery, clippy::pedantic, rust_2018_idioms)]
+#![allow(
+    clippy::empty_enum,
+    clippy::iter_nth_zero,
+    clippy::match_bool,
+    clippy::missing_errors_doc,
+    clippy::module_name_repetitions
+)]
+// To use the `unsafe` keyword, change to `#![allow(unsafe_code)]` (do not remove); aids auditing.
+#![forbid(unsafe_code)]
+
+// Uncomment before ship to reconcile use of possibly redundant crates, debug remnants, missing
+// license files and more
+//#![warn(clippy::cargo, clippy::restriction, missing_docs, clippy::missing_errors_doc, warnings)]
+//#![deny(warnings)]
+
+use {lib::{self, Args, error::Result}};
+use structopt::StructOpt;
+
+fn main() -> Result<()> {
+    lib::main(Args::from_args())
+}
